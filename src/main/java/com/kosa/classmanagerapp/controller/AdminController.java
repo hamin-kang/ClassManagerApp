909632ca6653d8b0786b767fb705f62caef03f89
leeahyoen01 <leeahyeon01@gmail.com>
2025-11-26 13:37:41 +0900

    team dropbox

--- src/main/java/com/kosa/classmanagerapp/controller/AdminController.java
+++ src/main/java/com/kosa/classmanagerapp/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.kosa.classmanagerapp.controller;
 
 import com.kosa.classmanagerapp.MainApplication;
+import javafx.application.Platform;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
@@ -37,7 +39,28 @@ public class AdminController {
 
     }
 
+    @FXML
+    private ComboBox<String> teamComboBox;
 
+
+    private void loadTeamList() {
+        // 실제로 팀을 DB나 서비스에서 가져오는 것처럼 구성 가능
+        // 여기서는 예제로 1~5팀 추가
+        teamComboBox.getItems().addAll(
+                "1팀", "2팀", "3팀", "4팀", "5팀"
+        );
+
+        // 기본 선택값
+        teamComboBox.getSelectionModel().selectFirst();
+    }
+
+
+    @FXML
+    public void initialize() {
+        //fxml 로딩 후 안전하게 실행
+
+        Platform.runLater(() -> loadTeamList());
+    }
 
 
 }
