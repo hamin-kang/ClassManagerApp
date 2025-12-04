4d533c2b379d45a124181c7794b37c547d5bbe22
leeahyoen01 <leeahyeon01@gmail.com>
2025-12-04 10:50:05 +0900

    과제리스트 생성완료

--- src/main/java/com/kosa/classmanagerapp/model/BaseEntity.java
+++ src/main/java/com/kosa/classmanagerapp/model/BaseEntity.java
@@ -5,7 +5,15 @@ public class BaseEntity {
 
     public Long getId() {
         return id;
+
     }
+
+    // int형으로 받아오기
+    public int getIdInt() {
+        return id != null ? id.intValue() : 0; // Long → int 변환
+    }
+
+
 
     public void setId(Long id) {
         this.id = id;
