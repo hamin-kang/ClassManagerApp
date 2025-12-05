ceede43d7017f4db1c7ead61c4a702e147b3f2c5
DooHyoJeong <doohj1218@naver.com>
2025-12-05 11:07:04 +0900

    [chore] init 데이터 수정

--- src/main/java/com/kosa/classmanagerapp/model/entity/User.java
+++ src/main/java/com/kosa/classmanagerapp/model/entity/User.java
@@ -28,7 +28,74 @@ public class User {
     // created_at DATETIME -> createdAt
     private LocalDateTime createdAt;
 
+    public User() {}
+    // Builder 내부 클래스
+    public static class Builder {
+        private Long id;
+        private String userName;
+        private String passwordHash;
+        private String fullName;
+        private Long teamId;
+        private LocalDate birthday;
+        private UserAuthorization authorization;
+        private LocalDateTime createdAt;
 
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder userName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public Builder passwordHash(String passwordHash) {
+            this.passwordHash = passwordHash;
+            return this;
+        }
+
+        public Builder fullName(String fullName) {
+            this.fullName = fullName;
+            return this;
+        }
+
+        public Builder teamId(Long teamId) {
+            this.teamId = teamId;
+            return this;
+        }
+
+        public Builder birthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Builder authorization(UserAuthorization authorization) {
+            this.authorization = authorization;
+            return this;
+        }
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.id = this.id;
+            user.userName = this.userName;
+            user.passwordHash = this.passwordHash;
+            user.fullName = this.fullName;
+            user.teamId = this.teamId;
+            user.birthday = this.birthday;
+            user.authorization = this.authorization;
+            user.createdAt = this.createdAt;
+            return user;
+        }
+    }
+    public static Builder builder() {
+        return new Builder();
+    }
     public Long getId() {
         return id;
     }
